74fb9c1ae936c0b378768b0d01c2b78c3bf73b68
parksoong <162438646+parksoong@users.noreply.github.com>
2024-12-04 20:14:14 +0900

    Ex08_01.java

--- week03/src/week03/Ex08_01.java
+++ week03/src/week03/Ex08_01.java
@@ -2,13 +2,13 @@ package week03;
 
 public class Ex08_01 {
 	public static void main(String[] args) {
-		printHello();
+		printHello();//printHello라는 메소드 호출(같은 main 메소드 안에서 작성된 함수라 메소드 클래스를 생성하지 x)
 	}
 	
-	static void printHello() {
+	static void printHello() {//static void로 printHello라는 메소드를 생성
 		
-		System.out.println("안녕하세요");
-		System.out.println("만나서 반갑습니다.");
+		System.out.println("안녕하세요");//""안의 문자를 출력하고 줄바꿈
+		System.out.println("만나서 반갑습니다.");//""안의 문자를 출력하고 줄바꿈
 	}
 
 }
